71d6c4cc8d91cf74b295fae04678e4bb5f439de5
Henrique <henri.eder@outlook.com>
2022-11-30 00:33:54 -0300

    inicio doi projeto

---
+++ src/main/java/com/projetonosso/projetonosso/ProjetonossoApplication.java
@@ -0,0 +1,13 @@
+package com.projetonosso.projetonosso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetonossoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetonossoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetonosso/projetonosso/ProjetonossoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetonosso.projetonosso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetonossoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
