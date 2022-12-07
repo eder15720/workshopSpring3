398e8e05f22b81f329283251365c622c5499cb6b
Henrique <henri.eder@outlook.com>
2022-12-07 01:00:44 -0300

    service camada para cada componente da interface

--- src/main/java/com/projetonosso/projetonosso/resources/UserResource.java
+++ src/main/java/com/projetonosso/projetonosso/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.projetonosso.projetonosso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetonosso.projetonosso.entities.User;
+import com.projetonosso.projetonosso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Mario", "mario@mario.com.br", "11952655452", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> listUsers = userService.findAll();
+		return ResponseEntity.ok().body(listUsers);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
+ 
