e96560d328c01b6bb4684c8b5306e589d1e6a450
Henrique <henri.eder@outlook.com>
2023-01-04 00:37:31 -0300

    adição da classe de pagaentos por item

--- src/main/java/com/projetonosso/projetonosso/config/TestConfig.java
+++ src/main/java/com/projetonosso/projetonosso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetonosso.projetonosso.entities.Category;
 import com.projetonosso.projetonosso.entities.Order;
 import com.projetonosso.projetonosso.entities.OrderItem;
+import com.projetonosso.projetonosso.entities.Payment;
 import com.projetonosso.projetonosso.entities.Product;
 import com.projetonosso.projetonosso.entities.User;
 import com.projetonosso.projetonosso.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
