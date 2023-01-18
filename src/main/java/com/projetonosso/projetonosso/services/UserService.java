4422c6a73aaea490b12a2b8a05f6cd14e5e22320
Henrique <henri.eder@outlook.com>
2023-01-18 01:39:36 -0300

    tratamento de exceções para erros no request mapping

--- src/main/java/com/projetonosso/projetonosso/services/UserService.java
+++ src/main/java/com/projetonosso/projetonosso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projetonosso.projetonosso.entities.User;
 import com.projetonosso.projetonosso.repositories.UserRepository;
+import com.projetonosso.projetonosso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
