f007dceffeb4c6e150f8e6b9fd58f17dd6b87d2a
Henrique <henri.eder@outlook.com>
2023-01-19 00:46:22 -0300

    adiconando tratativa de erros para deletar usuários

--- src/main/java/com/projetonosso/projetonosso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetonosso/projetonosso/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.projetonosso.projetonosso.services.exceptions.DataBaseException;
 import com.projetonosso.projetonosso.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +18,15 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> DataBaseException(DataBaseException e, HttpServletRequest request){
+		String error = "Database Error";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		
